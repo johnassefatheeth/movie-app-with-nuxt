9566c9a6614ce61e9618ef8959f2bddf3bcd183b
John Assefa <johnassefaforbussines@gmail.com>
2024-05-31 02:29:40 -0700

    styling home component

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,13 @@
     <div class="text-gray-400">
         <Navbar/>
         <Home/>
+        <div class="bg-gradient-to-r from-cyan-500 to-blue-500 p-12">
+            <div class="flex w-full bg-slate-600 p-2">         
+                Recent
+                <hr class="m-4 w-full border-1 border-white mt-4">
+            </div>
+        </div>
+        
     </div>
 </template>
 
