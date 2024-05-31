e302e46fca902dce28a3ffd8677f2357bf34270a
John Assefa <johnassefaforbussines@gmail.com>
2024-05-31 03:02:33 -0700

    creating basinc structure

---
+++ components/Movie.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-[200px] h-[300px] rounded-lg inline-block m-4">
+        <img src="https://rukminim2.flixcart.com/image/850/1000/l01blow0/poster/v/p/r/medium-the-avengers-a-on-good-quality-hd-quality-wallpaper-original-imagbx2pbrnumdve.jpeg?q=90&crop=false" alt="" class="w-full h-full object-fill">
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ components/MoviesList.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="bg-gradient-to-r from-black to-gray-900 p-12 text-white ">
+        <h2 class="text-center text-2xl">Movies</h2>
+            <div class="flex w-full p-2">         
+                Recent
+                <hr class="m-4 w-full border-1 border-white mt-4">
+            </div>
+            <div class="w-full  px-14 ">
+                <Movie/><Movie/><Movie/>
+                <Movie/><Movie/>
+            </div>
+            <div class="flex w-full  p-2">         
+                Top
+                <hr class="m-4 w-full border-1 border-white mt-4">
+            </div>
+            <div class="w-full  px-14 ">
+                <Movie/><Movie/><Movie/>
+                <Movie/><Movie/>
+            </div>
+
+            <div class="flex w-full  p-2">         
+                Legendary
+                <hr class="m-4 w-full border-1 border-white mt-4">
+            </div>
+            <div class="w-full  px-14 ">
+                <Movie/><Movie/><Movie/>
+                <Movie/><Movie/>
+            </div>
+
+        </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
