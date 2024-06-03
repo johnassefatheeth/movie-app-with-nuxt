49cc93ef71e86b3204a64a2063d55424c1cf58a0
John Assefa <johnassefaforbussines@gmail.com>
2024-06-03 01:45:53 -0700

    fetching series api data

--- components/SeriesList.vue
+++ components/SeriesList.vue
@@ -2,24 +2,24 @@
     <div class="bg-gradient-to-r from-black to-gray-900 p-12 text-white ">
         <h2 class="text-center text-2xl">Series</h2>
             <div class="flex w-full p-2">         
-                Recent
-                <hr class="m-4 w-full border-1 border-white mt-4">
-            </div>
-            <Lister/>
-
-            <div class="flex w-full  p-2">         
-                Ongoing
-                <hr class="m-4 w-full border-1 border-white mt-4">
-            </div>
-            
-            <Lister/>
-
-            <div class="flex w-full  p-2">         
                 Top Rated
                 <hr class="m-4 w-full border-1 border-white mt-4">
             </div>
+            <Lister />
+
+            <div class="flex w-full  p-2">         
+                popular
+                <hr class="m-4 w-full border-1 border-white mt-4">
+            </div>
+            
+            <Lister filterType="popular"/>
+
+            <div class="flex w-full  p-2">         
+                Airing Today
+                <hr class="m-4 w-full border-1 border-white mt-4">
+            </div>
            
-            <Lister/>
+            <Lister filterType="airing_today"/>
 
         </div>
 </template>
