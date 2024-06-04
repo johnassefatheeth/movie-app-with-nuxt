2392d0726e85947942f4344c159736998ab0b1aa
John Assefa <johnassefaforbussines@gmail.com>
2024-06-04 02:16:48 -0700

    adding hover effect

--- components/Lister.vue
+++ components/Lister.vue
@@ -3,12 +3,20 @@
     <div v-if="data === null">Loading...</div>
     <div v-else class="flex flex-wrap">
       <!-- Handle the case for the 'latest' filter type -->
-      <div v-if="props.filterType === 'latest'" class="bg-black flex-shrink-0 m-2">
-        <img :src="`https://image.tmdb.org/t/p/w500${data.poster_path}`" class="w-[200px]">
+      <div v-if="props.filterType === 'latest'" class="relative bg-black flex-shrink-0 m-2 hover-wrapper">
+        <img :src="`https://image.tmdb.org/t/p/w500${data.poster_path}`" class="w-[200px] transition-opacity duration-300 ease-in-out hover:opacity-40">
+        <div class="absolute top-0 right-0 bg-red-600 text-white p-1 text-sm opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out">{{ data.vote_average.toFixed(1) }}</div>
+        <div class="absolute bottom-0 left-0 right-0 p-2 text-white text-sm opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out bg-black bg-opacity-50 hover:bottom-0" style="height: 65%;">
+          {{ data.overview }}
+        </div>
       </div>
       <!-- Handle the case for other filter types -->
-      <div v-else v-for="(show, index) in displayedShows" :key="show.id" class="bg-black flex-shrink-0 m-2">
-        <img :src="`https://image.tmdb.org/t/p/w500${show.poster_path}`" class="w-[200px]">
+      <div v-else v-for="(show, index) in displayedShows" :key="show.id" class="relative bg-black flex-shrink-0 m-2 hover-wrapper">
+        <img :src="`https://image.tmdb.org/t/p/w500${show.poster_path}`" class="w-[200px] transition-opacity duration-300 ease-in-out hover:opacity-40">
+        <div class="absolute top-0 right-0 bg-red-600 text-white p-1 text-sm opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out">{{ show.vote_average.toFixed(1) }}</div>
+        <div class="absolute bottom-[-65%] left-0 right-0 p-2 text-white text-sm opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out bg-black bg-opacity-50 hover:bottom-0" style="height: 65%;">
+          {{ show.overview }}
+        </div>
       </div>
     </div>
     <!-- Conditionally render the button only if not showing 'latest' -->
@@ -68,3 +76,26 @@ const displayedShows = computed(() => {
   return showAll.value ? data.value : data.value.slice(0, 6);
 });
 </script>
+
+<style scoped>
+.hover-wrapper {
+  position: relative;
+}
+
+.hover-wrapper img:hover {
+  opacity: 0.4;
+}
+
+.hover-wrapper .absolute {
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out, bottom 0.3s ease-in-out;
+}
+
+.hover-wrapper:hover .absolute {
+  opacity: 1;
+}
+
+.hover-wrapper .description {
+  height: 65%;
+}
+</style>
