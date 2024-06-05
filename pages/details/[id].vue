78b7f781b0b78c54798f7c4e3fb4eccaa5462563
John Assefa <johnassefaforbussines@gmail.com>
2024-06-04 23:18:26 -0700

    editing details

--- pages/details/[id].vue
+++ pages/details/[id].vue
@@ -1,15 +1,25 @@
 <template>
     <div>
       <div v-if="movie">
-        {{ movie }}
-        <!-- <h1>{{ movie.title }}</h1>
+        <h1>{{ movie.title }}</h1>
         <p>{{ movie.overview }}</p>
         <img :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path" alt="Movie Poster">
         <p><strong>Release Date:</strong> {{ movie.release_date }}</p>
         <p><strong>Rating:</strong> {{ movie.vote_average }}</p>
+        <div v-if="trailer">
+          <h2>Trailer</h2>
+          <iframe
+            :src="'https://www.youtube.com/embed/' + trailer.key"
+            width="560"
+            height="315"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+          </iframe>
+        </div>
       </div>
       <div v-else>
-        <p>Loading...</p> -->
+        <p>Loading...</p>
       </div>
     </div>
   </template>
@@ -21,6 +31,7 @@
   
   const router = useRoute();
   const movie = ref(null);
+  const trailer = ref(null);
   
   const fetchMovieDetails = async (id) => {
     try {
@@ -35,8 +46,24 @@
     }
   };
   
+  const fetchMovieTrailer = async (id) => {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/videos`, {
+        params: {
+          api_key: '83393ab8ab75bb658f2250fece439a12'  // Replace with your actual TMDB API key
+        }
+      });
+      const trailers = response.data.results;
+      trailer.value = trailers.find(video => video.type === 'Trailer' && video.site === 'YouTube');
+    } catch (error) {
+      console.error('Error fetching movie trailer:', error);
+    }
+  };
+  
   onMounted(() => {
-    fetchMovieDetails(router.params.id);
+    const movieId = router.params.id;
+    fetchMovieDetails(movieId);
+    fetchMovieTrailer(movieId);
   });
   </script>
   
