40ec27c0fd62dd2e2ae72f480081f61e7adbe51a
John Assefa <johnassefaforbussines@gmail.com>
2024-06-10 03:44:04 -0700

    styling details page

--- pages/details/[id].vue
+++ pages/details/[id].vue
@@ -1,20 +1,22 @@
 <template >
     <div class="bg-gradient-to-r from-gray-900 to-black text-white">
-      <div v-if="movie" > 
-        {{ movie }}
-        <img :src="'https://image.tmdb.org/t/p/w500' + movie.backdrop_path" alt="Movie Poster" class="w-full h-[400px] object-fit bg-blend-overlay">
+      <div v-if="movie"  class="p-12 "> 
+        
+        <h1 class="text-6xl font-bold font-mono text-center mb-4 ">{{ movie.title }}</h1>
+        <img :src="'https://image.tmdb.org/t/p/w500' + movie.backdrop_path" alt="Movie Poster" class="w-[500px]  object-cover ">
         <!-- <div class="w-full h-[400px] bg-gradient-to-b from-black absolute top-0"></div> -->
-
-        <h1 class="text-2xl font-bold font-mono">{{ movie.title }}</h1>
-        <div class="flex w-full">
+      <div class="mt-2 w-[500px]">
+        <div class="flex w-full ">
             <div v-for="gener in movie.genres" :key="movie.genres.id">
             <div class="bg-gray-700 w-fit m-3 p-2 rounded-lg">{{gener.name}}</div>
         </div>
         </div>
         
-        <p class="font-light w-[300px]">{{ movie.overview }}</p>
-        <p><strong>Release Date:</strong> {{ movie.release_date }}</p>
-        <p><strong>Rating:</strong> {{ movie.vote_average }}</p>
+        
+            <p class="font-light">{{ movie.overview }}</p>
+            <p><strong>Release Date:</strong> {{ movie.release_date }}</p>
+            <p><strong>Rating:</strong> {{ movie.vote_average }}</p>
+        </div>
         <div v-if="trailer">
           <h2 class="text-xl">watch Trailer</h2>
           <iframe
