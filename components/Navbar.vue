ad46aa47c8b6ae8cfeb83f844c52be1bdc4b798a
John Assefa <johnassefaforbussines@gmail.com>
2024-06-11 02:22:39 -0700

    editing series details page incldues details2

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,46 +1,81 @@
 <template>
- <div
-      @mouseleave="hoverOnBrowse = false"
-      class="absolute opacity-80 z-20 w-full h-20 bg-gray-800 top-0 transition-all duration-300 ease-in-out hover:opacity-100 "
-    > 
-      <div class="w-1/5  h-20 float-left">
-        <!-- <img class="h-full ml-4" src="../assets/image/food.png" alt=""> -->
-      </div>
-      <ul class="flex justify-evenly items-center h-full float-right mr-4">
-        <li
-          @mouseover="hoverOnBrowse = false"
-          class="hover:text-white transition-all duration-300 ease-in-out m-4 mr-8"
-        >
-          <NuxtLink to="/" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">HOME</NuxtLink>
-        </li>
-        <li
-          class=" m-4 hover:text-white transition-all duration-300 ease-in-out mr-8"
-        >
-          <NuxtLink to="/movies" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">MOVIES</NuxtLink>
-        </li>
-        <li
-          class=" m-4 hover:text-white transition-all duration-300 ease-in-out mr-8"
-        >
-          <NuxtLink to="/series" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">SERIES</NuxtLink>
-        </li>
-        <li
-          class=" m-4 hover:text-white transition-all duration-300 ease-in-out mr-8"
-        >
-          <NuxtLink to="/about" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">ABOUT</NuxtLink>
-        </li>
-        <li>
-            <div class="w-[300px] h-[30px] bg-white rounded-full flex">
-                <svg width="28px" height="28px" viewBox="0 0 24 24" class="ml-2" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-                <p class="ml-2">|</p>
-                <input type="text" placeholder="search" class="h-full float-end w-[250px] rounded-full pb-1 pl-8">
-            </div>
-        </li>
-        
-    </ul>
-        
+  <div
+    @mouseleave="hoverOnBrowse = false"
+    class="absolute opacity-80 z-20 w-full h-20 bg-gray-800 top-0 transition-all duration-300 ease-in-out hover:opacity-100"
+  >
+    <div class="w-1/5 h-20 float-left">
+      <!-- <img class="h-full ml-4" src="../assets/image/food.png" alt=""> -->
     </div>
+    <ul class="flex justify-evenly items-center h-full float-right mr-4">
+      <li
+        @mouseover="hoverOnBrowse = false"
+        class="hover:text-white transition-all duration-300 ease-in-out m-4 mr-8"
+      >
+        <NuxtLink to="/" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">HOME</NuxtLink>
+      </li>
+      <li class="m-4 hover:text-white transition-all duration-300 ease-in-out mr-8">
+        <NuxtLink to="/movies" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">MOVIES</NuxtLink>
+      </li>
+      <li class="m-4 hover:text-white transition-all duration-300 ease-in-out mr-8">
+        <NuxtLink to="/series" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">SERIES</NuxtLink>
+      </li>
+      <li class="m-4 hover:text-white transition-all duration-300 ease-in-out mr-8">
+        <NuxtLink to="/about" class="text-gray-300 hover:text-white transition-all duration-300 ease-in-out">ABOUT</NuxtLink>
+      </li>
+      <li>
+        <div class="relative">
+          <div class="w-[300px] h-[30px] bg-white rounded-full flex">
+            <svg width="28px" height="28px" viewBox="0 0 24 24" class="ml-2" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            <p class="ml-2">|</p>
+            <input
+              type="text"
+              placeholder="search"
+              class="h-full float-end w-[250px] rounded-full pb-1 pl-8"
+              v-model="searchQuery"
+              @input="searchContent"
+            >
+          </div>
+          <div v-if="searchResults.length > 0" class="absolute bg-white w-[300px] mt-1 rounded-lg shadow-lg">
+            <div v-for="result in searchResults" :key="result.id" class="p-2 border-b hover:bg-gray-200">
+              <NuxtLink :to="`/${result.media_type}/${result.id}`">
+                {{ result.title || result.name }}
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
 
+const searchQuery = ref('');
+const searchResults = ref([]);
+
+const searchContent = async () => {
+  if (searchQuery.value.length > 2) {
+    try {
+      const response = await axios.get(`https://api.themoviedb.org/3/search/multi`, {
+        params: {
+          api_key: '83393ab8ab75bb658f2250fece439a12', // Replace with your actual TMDB API key
+          query: searchQuery.value,
+        },
+      });
+      searchResults.value = response.data.results.filter(
+        result => result.media_type === 'movie' || result.media_type === 'tv'
+      );
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  } else {
+    searchResults.value = [];
+  }
+};
 </script>
+
+<style scoped>
+/* Add any additional styles here */
+</style>
